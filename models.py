0930b85ba11fc7be499a25d79c4c6fe0319fcd84
nahe-consulting-group <66278632+nahe-consulting-group@users.noreply.github.com>
2020-10-27 20:07:01 -0300

    Update models.py

--- models.py
+++ models.py
@@ -5,6 +5,8 @@ class ProductTemplate(models.Model):
 
     @api.model
     def update_mdq(self):
-        products = self.env['product.product'].search([('id','=',7945)])
+        products = self.env['product.product'].search([('name','=','ADAPTADOR BLUETOOTH 4.0 USB - OEM TP-7183')])
+        #products = self.env['product.product'].search([('currency_id','=',1)])
         for product in products:
             product.list_price = product.list_price / 90
+            product.force_currency_id = 3
