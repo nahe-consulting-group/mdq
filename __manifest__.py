d812dc8fa60adc7b1cb6592803dd024ff9920824
nahe-consulting-group <66278632+nahe-consulting-group@users.noreply.github.com>
2020-10-27 20:18:15 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'MDQ',
+    'name': 'update-currency-and-price',
     'version': '1.0',
     'category': 'Tools',
-    'summary': 'MDQ',
+    'summary': 'update-currency-and-price TO DO ',
     'depends': ['sale','product'],
     'data': [
         ],
